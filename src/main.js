89a65f14665205b9885c1cf8059dffa45f7001b0
zjx <2509330536@qq.com>
2022-02-17 22:30:05 +0800

    'first'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//导入element
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+//导入axios
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(Axios,VueAxios)
+
 Vue.config.productionTip = false
 
 new Vue({
